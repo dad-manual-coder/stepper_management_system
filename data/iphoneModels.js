8d4a08416e377fa95d38df22f6e9b1815b21e69e
Ousman Sankareh <166555322+dad-manual-coder@users.noreply.github.com>
2025-10-14 17:17:31 +0000

    Create iphoneModels.js

---
+++ data/iphoneModels.js
@@ -0,0 +1,10 @@
+// data/iphoneModels.js
+
+export const iphoneModels = [
+  "iPhone 4", "iPhone 4s", "iPhone 5", "iPhone 5s", "iPhone 6", "iPhone 6s", "iPhone 6s Plus",
+  "iPhone 7", "iPhone 7 Plus", "iPhone 8", "iPhone 8 Plus", "iPhone X", "iPhone XR", "iPhone XS",
+  "iPhone XS Max", "iPhone 11", "iPhone 11 Pro", "iPhone 11 Pro Max", "iphone 12 mini", "iPhone 12", "iPhone 12 Pro",
+  "iPhone 12 Pro Max", "iphone 13 Mini", "iPhone 13", "iPhone 13 Pro", "iPhone 13 Pro Max", "iPhone 14", "iPhone 14 Pro",
+  "iPhone 14 Pro Max", "iPhone 15", "iPhone 15 Pro", "iPhone 15 Pro Max", "iphone 16 E", "iPhone 16", "iPhone 16 Pro",
+  "iPhone 16 Pro Max", "iPhone 17", "iphone 17 Air", "iPhone 17 Pro", "iPhone 17 Pro Max"
+];
